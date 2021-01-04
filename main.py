fb306ed1ca97a58b718d016432b8c5657bd44623
meherett <meherett@zoho.com>
2021-01-04 17:10:01 +0300

    Fix: CBlock filter bugs and update all components.

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 """
 Title        : QtumOB - (Qtum Orphan Block)
-Description  : Qtum blockchain orphan block finder for all Qtum core debug files.
+Description  : Qtum blockchain orphan blocks finder for all Qtum core debug files.
 Author       : Meheret Tesfaye Batu <meherett@zoho.com>
 """
 
@@ -14,9 +14,12 @@ from tqdm import tqdm
 import requests
 import json
 import warnings
+import sys
 
+# Constant values
+filters = ["UpdateTip", "CBlock"]
 # UpdateTip, ThreadStakeMiner
-_ = ("UpdateTip", "ThreadStakeMiner", "CreateNewBlock", "CBlock")
+_ = ("UpdateTip", "ThreadStakeMiner", "CreateNewBlock", "CBlock", "CBlockFileInfo")
 # Ignore all warnings
 warnings.filterwarnings('ignore')
 
@@ -35,43 +38,33 @@ class QtumOB:  # Qtum Orphan Block
             debug_data.close()
         return lines
 
-    def filter_lines(self, lines: List[str], filters: List[str]) -> "QtumOB":
+    def filter_lines(self, lines: List[str], _filters: List[str]) -> "QtumOB":
         for index, line in enumerate(lines):
-            for _filter in filters:
+            for _filter in _filters:
                 if _filter in line and _filter in _[0]:
                     split_debug: List[str] = line[:-1].split(" ")
                     length_split_debug: int = len(split_debug)
-                    if length_split_debug == 21 and _filter in line and _[2] not in line:
-                        data = dict()
+                    if length_split_debug == 21 and _[1] in line and _[2] not in line:
+                        data: dict = {}
                         for i, detail_split_debug in enumerate(split_debug):
                             if i == 13:
                                 _split_debug = detail_split_debug.split("=")
                                 data.setdefault("Hash", _split_debug[1])
-                            # elif i in [14, 15]:
-                            #     _split_debug = detail_split_debug.split("=")
-                            #     data.setdefault(_split_debug[0], _split_debug[1])
                         self.datas.append(data)
                     elif length_split_debug == 11:
-                        data = dict()
+                        data: dict = {}
                         for i, detail_split_debug in enumerate(split_debug):
                             if i == 3:
                                 _split_debug = detail_split_debug.split("=")
                                 data.setdefault("Hash", _split_debug[1])
-                            # elif i in [4, 5]:
-                            #     _split_debug = detail_split_debug.split("=")
-                            #     data.setdefault(_split_debug[0], _split_debug[1])
                         self.datas.append(data)
-                elif _filter in line and _filter in _[3]:
+                elif _filter in line and _filter in _[3] and _[4] not in line:
                     split_debug: List[str] = line[:-1].split(" ")
                     length_split_debug: int = len(split_debug)
                     if length_split_debug == 15 and _filter in line:
-                        data = dict(date=split_debug[0], update_tip=False, cblock=True)
-                        # for i, detail_split_debug in enumerate(split_debug):
-                        #     if i in [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]:
-                        #         continue
-                        #     _split_debug = detail_split_debug.split("=")
-                        #     data.setdefault(_split_debug[0], _split_debug[1])
-                        # self.datas.append(data)
+                        data: dict = {}
+                        data.setdefault("Hash", split_debug[1][-65:].rstrip(","))
+                        self.datas.append(data)
         return self
 
 
@@ -80,20 +73,32 @@ def main(config: dict):
     for debug in config["debugs"]:
         qtum_ob: QtumOB = QtumOB(config)
         lines: List[str] = qtum_ob.read_lines(name=debug["debug_name"])
-        qtum_ob.filter_lines(lines=lines, filters=debug["filters"])
+        qtum_ob.filter_lines(lines=lines, _filters=(debug["filters"] if "filters" in debug else filters))
         data_frame: DataFrame = DataFrame(qtum_ob.datas)
         tqdm.pandas(desc=f"{debug['debug_name']}")
-        data_frame[["Orphan Block", "Block Height"]] = data_frame[0:3]['Hash'].progress_apply(is_orphan_block)
-        excel_writer = ExcelWriter(f"{debug['save_name']}.xlsx", engine="xlsxwriter")
-        data_frame.to_excel(excel_writer, sheet_name='SheetQtumOB', index=False, na_rep="None")
-        _format = excel_writer.book.add_format({'align': 'center'})
-        excel_writer.sheets['SheetQtumOB'].set_column(
-            data_frame.columns.get_loc('Hash'), data_frame.columns.get_loc('Hash'), 70, _format
+        try:
+            start_index = debug["start_index"] if "start_index" in debug else 0
+            if "stop_index" in debug:
+                data_frame[["Orphan Block", "Block Height"]] = (
+                    data_frame[start_index:debug["stop_index"]]["Hash"].progress_apply(is_orphan_block)
+                )
+            else:
+                data_frame[["Orphan Block", "Block Height"]] = (
+                    data_frame[start_index:]["Hash"].progress_apply(is_orphan_block)
+                )
+        except KeyError:
+            print("There is no Qtum block hash.")
+            sys.exit()
+        excel_writer = ExcelWriter(f"{config['debugs_dir']}/{debug['save_name']}.xlsx", engine="xlsxwriter")
+        data_frame.to_excel(excel_writer, sheet_name="SheetQtumOB", index=False, na_rep="-")
+        _format = excel_writer.book.add_format({"align": "center"})
+        excel_writer.sheets["SheetQtumOB"].set_column(
+            data_frame.columns.get_loc("Hash"), data_frame.columns.get_loc("Hash"), 70, _format
         )
-        excel_writer.sheets['SheetQtumOB'].set_column(
-            data_frame.columns.get_loc('Orphan Block'), data_frame.columns.get_loc('Orphan Block'), 15, _format
+        excel_writer.sheets["SheetQtumOB"].set_column(
+            data_frame.columns.get_loc("Orphan Block"), data_frame.columns.get_loc("Orphan Block"), 15, _format
         )
-        excel_writer.sheets['SheetQtumOB'].set_column(
+        excel_writer.sheets["SheetQtumOB"].set_column(
             data_frame.columns.get_loc("Block Height"), data_frame.columns.get_loc("Block Height"), 15, _format
         )
         excel_writer.save()
@@ -110,7 +115,7 @@ if __name__ == '__main__':
 
     # Check Qtum network type
     if CONFIG["network"] not in ["mainnet", "testnet"]:
-        raise ValueError("Invalid Qtum network, Please choose only 'mainnet' or 'testnet' networks.")
+        raise ValueError("Invalid Qtum network, Please choose only mainnet or testnet networks.")
 
     # Check orphan block check on Qtum blockchain
     def is_orphan_block(block_id: str, headers: dict = CONFIG["headers"],
